fa30d4c1a602840c6efc2417057d977fa473da47
KelvinYou <ykekelvin0220@gmail.com>
2023-02-28 13:36:08 +0800

    transition

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,6 @@ import { ThemeContext } from './../theme/ThemeContext';
 const Home = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  console.info(theme.primaryColor + ", " + theme.secondaryColor);
-
   return (
     <div style={{ 
       backgroundColor: theme.backgroundColor,
@@ -18,7 +16,19 @@ const Home = () => {
       <div  style={{ color: theme.onBackgroundColor }}>
         Home
       </div>
-      <button onClick={toggleTheme}>
+      <button
+        onClick={toggleTheme}
+        style={{
+          padding: "10px 20px",
+          borderRadius: "4px",
+          backgroundColor: theme.secondaryColor,
+          color: theme.onSecondaryColor,
+          border: "none",
+          cursor: "pointer",
+          marginTop: "20px",
+          transition: "background-color 0.5s ease",
+        }}
+      >
         {theme.mode === "light" ? "Switch to Dark" : "Switch to Light"}
       </button>
     </div>
